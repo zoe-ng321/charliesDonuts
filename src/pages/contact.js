7c6398f07f0aef30722b67c8fb97350d99b2ce72
zoeng <zoe.ng@utexas.edu>
2020-10-28 18:32:16 -0500

    add map to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ import json from '../content/contact.json'
 import Button from 'react-bootstrap/Button'
 import {BsChevronRight} from 'react-icons/bs'
 import Img from "gatsby-image"
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoBox } from '@react-google-maps/api';
 
 
 
@@ -29,29 +29,31 @@ export default function Contact({data}) {
           center={center}
           zoom={17}
         >
-          <Marker
-            position={center}
-          />
-        </GoogleMap>
+          <InfoBox position={center}>
+            <div style={{ backgroundColor: 'lightgrey', padding: 10 }}>
+              <div style={{ fontSize: 14, color: 'black' }}>
+                Charlie's Donuts
+              </div>
+            </div>
+          </InfoBox>
+          <Marker position={center}/>
+          </GoogleMap>
       </LoadScript>
   );
   return (
     <div>
       <NavBar/>
       <div className="container">
-       <h1>{json.title}</h1>
-       {map}
-       <Img
-         fluid={data.map.childImageSharp.fluid}
-         alt="map of charlie's donuts"
-         className="image"
-       />
-      <div style={{marginTop: '1rem'}}>
-        <p>{json.content[0].item}<br/><a href="https://www.google.com/maps/dir//Charlie's+Donuts,+1406B+E+Rio+Grande+St,+Victoria,+TX+77901/@28.8068871,-96.9891038,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x864266c3348d575d:0xd250a4e6a586dc63!2m2!1d-96.9891038!2d28.8068871!3e0" target="_blank" rel="noopener noreferrer"><b>Get Directions</b></a></p>
-        <p>{json.content[1].item}</p>
-        <p>{json.content[2].item}</p>
-        <p>{json.content[3].item}</p>
-      </div>
+        <h1>{json.title}</h1>
+        <div style={{alignItems:'center'}}>
+        {map}
+        </div>
+        <div style={{marginTop: '1rem'}}>
+          <p>{json.content[0].item}<br/><a href="https://www.google.com/maps/dir//Charlie's+Donuts,+1406B+E+Rio+Grande+St,+Victoria,+TX+77901/@28.8068871,-96.9891038,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x864266c3348d575d:0xd250a4e6a586dc63!2m2!1d-96.9891038!2d28.8068871!3e0" target="_blank" rel="noopener noreferrer"><b>Get Directions</b></a></p>
+          <p>{json.content[1].item}</p>
+          <p>{json.content[2].item}</p>
+          <p>{json.content[3].item}</p>
+        </div>
       </div>
       <Footer/>
     </div>
