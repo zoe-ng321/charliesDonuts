a73335cd4732bee05c278f7cdc54a7e0459f8cf2
zoeng <zoe.ng@utexas.edu>
2020-11-05 16:18:28 -0600

    add title and icon

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,1 +1,4 @@
-module.exports = []
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Charlie's Donuts","short_name":"Charlie's Donuts","start_url":"/","background_color":"#f7f0eb","theme_color":"#a2466c","display":"browser","icon":"src/assets/donut4.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"ee306485d7350526d54b023421ecdc18"},
+    }]
