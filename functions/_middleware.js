f63cfc13cc9530de450f3f6fdf076f709ec2b082
zoeng <zoe.ng@utexas.edu>
2024-02-29 22:36:48 -0600

    fix middleware bug

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,17 +1,17 @@
 export async function onRequest(context) {
   try {
-    let resp = await fetch(request.url, request);
+    let resp = await fetch(context.request.url, context.request);
   
     let newResp = new Response(resp.body, {
       headers: resp.headers,
       status: resp.status
     })
   
-    if (request.url.endsWith(".js")) {
+    if (context.request.url.endsWith(".js")) {
       newResp.headers.set("Content-Type", "text/javascript");
     }
     return newResp;
-    
+
   } catch (err) {
     return new Response(`${err.message}\n${err.stack}`, { status: 500 });
   }
